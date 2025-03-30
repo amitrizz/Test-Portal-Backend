4ce942279df72ebe111951659cbabad8230f471d
amitrizz <amitsagar121001@gmail.com>
2025-03-30 12:12:23 +0530

    fix som api

--- TestPortal/src/main/java/com/springsecurity/examsets/service/imp/ExamSetServiceImp.java
+++ TestPortal/src/main/java/com/springsecurity/examsets/service/imp/ExamSetServiceImp.java
@@ -1,13 +1,9 @@
 package com.springsecurity.examsets.service.imp;
 
-import com.springsecurity.examnames.service.ExamService;
-import com.springsecurity.examsetquestion.service.ExamSetQuestionService;
 import com.springsecurity.examsets.model.ExamSet;
 import com.springsecurity.examsets.repository.ExamSetRepository;
 import com.springsecurity.examsets.service.ExamSetService;
 import com.springsecurity.user.exception.UserException;
-import com.springsecurity.userresultquestion.service.UserResultQuestionService;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,42 +12,24 @@ import java.util.List;
 @Service
 public class ExamSetServiceImp implements ExamSetService {
     private final ExamSetRepository examSetRepository;
-//    private final ExamService examService;
-//    private final ExamSetQuestionService examSetQuestionService;
-//    private final UserResultQuestionService userResultQuestionService;
-
-    public ExamSetServiceImp(ExamSetRepository examSetRepository, ExamService examService) {
+    public ExamSetServiceImp(ExamSetRepository examSetRepository) {
         this.examSetRepository = examSetRepository;
-//        this.examService = examService;
-//        this.examSetQuestionService = examSetQuestionService;
-//        this.userResultQuestionService = userResultQuestionService;
     }
 
     @Override
     public ExamSet createExamSet(ExamSet examSet) throws UserException {
-        ExamSet examSet1 = new ExamSet();
-//        if(!examService.isExamExist(examSet.getExamId())){
-//            throw new UserException("Exam Not Found");
-//        }
-        examSet1.setExamId(examSet.getExamId());
-        examSet1.setCreatedAt(LocalDateTime.now());
-        examSet1.setExamSetName(examSet.getExamSetName());
-        return examSetRepository.save(examSet1);
+        examSet.setCreatedAt(LocalDateTime.now());
+        return examSetRepository.save(examSet);
     }
 
     @Override
     public ExamSet updateExamSet(ExamSet examSet, Long examSetId) throws UserException {
-//        if(!examService.isExamExist(examSet.getExamId())){
-//            throw new UserException("Exam Not Found");
-//        }
         ExamSet examSet1 = examSetRepository.findById(examSetId).orElse(null);
         if (examSet1==null || !examSet1.getExamId().equals(examSet.getExamId())){
             throw new UserException("ExamSet Not Found or ExamName Not Match");
         }
-        examSet1.setExamId(examSet.getExamId());
-        examSet1.setCreatedAt(LocalDateTime.now());
-        examSet1.setExamSetName(examSet.getExamSetName());
-        return examSetRepository.save(examSet1);
+        examSet.setCreatedAt(LocalDateTime.now());
+        return examSetRepository.save(examSet);
     }
 
     @Override
